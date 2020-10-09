7a37d1d4d685010c20e75541b19471d9cdd5bf72
gennadytiunov <gennady.tiunov@gmail.com>
2020-10-09 20:33:23 +0300

    Grafana Alerts

--- prometheus-grafana/Curler.bat
+++ prometheus-grafana/Curler.bat
@@ -1,15 +1,15 @@
 
-CD c:\Work\OTUS\Labs\Helm\
+CD C:\Repositories\GitHub-ArchLabs\prometheus-grafana
 
 :LOOP
  
-ab.exe -n 100 -c 10 http://arch.homework/products > nul
+ab.exe -n 5000 -c 100 http://arch.homework/products > nul
 
-TIMEOUT 1 /nobreak > null
+TIMEOUT 1 /nobreak > nul
 
-ab.exe -n 100 -c 10 http://arch.homework/products/health > nul
+ab.exe -n 5000 -c 1000 http://arch.homework/products/health > nul
 
-TIMEOUT 1 /nobreak > null
+TIMEOUT 1 /nobreak > nul
 
 GOTO :LOOP
 
