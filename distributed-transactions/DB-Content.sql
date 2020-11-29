6bbe46cf650bc68665eae72b4e46568d9527f2d0
gennadytiunov <gennady.tiunov@gmail.com>
2020-11-29 21:36:25 +0300

    Saga Lab - DB structure, content and some notes changed.

---
+++ distributed-transactions/DB-Content.sql
@@ -0,0 +1,22 @@
+
+INSERT INTO Show (Name, Description) VALUES ('Avatar', 'A tribute to the world famous movie will astonish you with the marvelous acrobatic performance.')
+INSERT INTO Show (Name, Description) VALUES ('Wild Jungles', 'An exotic performance with the most dangerous wild beasts ever.')
+INSERT INTO Show (Name, Description) VALUES ('Russian Fairytales', 'A traditional Russian performance with bears on bikes and acrobats under circus dome.')
+
+INSERT INTO ShowSession (ShowId, Date, Price) VALUES (3, '2021-02-06 12:00:00', 2000)
+INSERT INTO ShowSession (ShowId, Date, Price) VALUES (2, '2021-02-06 15:00:00', 2500)
+INSERT INTO ShowSession (ShowId, Date, Price) VALUES (1, '2021-02-06 18:00:00', 3000)
+INSERT INTO ShowSession (ShowId, Date, Price) VALUES (3, '2021-02-07 12:00:00', 2000)
+INSERT INTO ShowSession (ShowId, Date, Price) VALUES (2, '2021-02-07 15:00:00', 2500)
+INSERT INTO ShowSession (ShowId, Date, Price) VALUES (1, '2021-02-07 18:00:00', 3000)
+
+INSERT INTO HallSeat (Row, Number) Values (1, 1)
+INSERT INTO HallSeat (Row, Number) Values (1, 2)
+INSERT INTO HallSeat (Row, Number) Values (1, 3)
+INSERT INTO HallSeat (Row, Number) Values (1, 4)
+INSERT INTO HallSeat (Row, Number) Values (1, 5)
+INSERT INTO HallSeat (Row, Number) Values (2, 1)
+INSERT INTO HallSeat (Row, Number) Values (2, 2)
+INSERT INTO HallSeat (Row, Number) Values (2, 3)
+INSERT INTO HallSeat (Row, Number) Values (2, 4)
+INSERT INTO HallSeat (Row, Number) Values (2, 5)
