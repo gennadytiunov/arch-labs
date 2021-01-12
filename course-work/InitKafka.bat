bec17e4de45485120dbf9671f2f26cfe4f0752c3
gennadytiunov <Gennady.Tiunov@gmail.com>
2021-01-12 23:31:36 +0300

    Saga Lab - notes changed.

--- course-work/InitKafka.bat
+++ course-work/InitKafka.bat
@@ -2,6 +2,8 @@ C:
 
 CD c:\Kafka\bin\windows\
 
+CALL kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic users
+
 CALL kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic bookings
 
 CALL kafka-topics.bat --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic payments
